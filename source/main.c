71831ff046e2425dc692474be868ddc14ff3a81b
EggMaster0906 <lsinpen@gmail.com>
2023-10-09 00:19:07 +0800

    first

---
+++ source/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("LLLLLLLLL\n");
+	printf("L\n");
+	printf("L\n");
+	printf("L\n");
+	printf("LL\n");
+	
+	printf("\n");
+
+	printf("X       X\n");
+	printf("  X   X \n");
+	printf("    X   \n");
+	printf("  X   X \n");
+	printf("X       X\n");
+
+	printf("\n");
+
+	printf("PPPPPPPPP\n");
+	printf("    P   P\n");
+	printf("    P   P\n");
+	printf("    P   P\n");
+	printf("      P  \n");
+
+	return 0;
+}
